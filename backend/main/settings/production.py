217ebb74d6720d1eebf0004ec8448a2343caa4b7
mngar <matiasngarcia2002@gmail.com>
2025-02-07 18:09:30 -0300

    docker-compose new

--- backend/main/settings/production.py
+++ backend/main/settings/production.py
@@ -25,10 +25,7 @@ DATABASES = {
         'USER': os.environ['DB_USER'],
         'PASSWORD': os.environ['DB_PASSWORD'],
         'HOST': os.environ['DB_HOST'],
-        'PORT': os.environ['DB_PORT'],
-        'OPTIONS': {
-            'unix_socket': '/var/run/mysqld/mysqld.sock',
-        }
+        'PORT': os.environ['DB_PORT']
     }
 }
 
